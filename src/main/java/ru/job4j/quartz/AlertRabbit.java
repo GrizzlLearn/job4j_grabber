e514c4aa542c311951348f6a20197d0fe3a7a99f
d.lebedev <d.lebedev@rabota.ru>
2023-09-14 01:33:01 +0300

    1.1. Job c параметрами [#260360] do

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -6,6 +6,7 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Optional;
 import java.util.Properties;
 
@@ -17,9 +18,11 @@ public class AlertRabbit {
 
     private static final String path = "./src/main/resources/app.properties";
     private final Properties config;
+    private final Connection cn;
 
-    public AlertRabbit(Properties config) {
+    public AlertRabbit(Properties config, Connection cn) {
         this.config = config;
+        this.cn = cn;
     }
 
     /**
@@ -27,11 +30,14 @@ public class AlertRabbit {
      * @see AlertRabbit#main(String[]) 
      * @param args Массив аргументов командной строки, переданных программе при запуске.
      * @throws IOException если происходит ошибка при чтении файла настроек.
+     * @throws ClassNotFoundException
+     * @throws SQLException
      * @see AlertRabbit#loadProp()
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         Properties cfg = loadProp();
-        AlertRabbit alertRabbit = new AlertRabbit(cfg);
+        Connection cn = dbConnection(cfg);
+        AlertRabbit alertRabbit = new AlertRabbit(cfg, cn);
         Optional<String> propVal = Optional.ofNullable(alertRabbit.config.getProperty("rabbit.interval"));
         int interval = 0;
 
@@ -42,7 +48,11 @@ public class AlertRabbit {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connect", cn);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
                     .repeatForever();
@@ -51,8 +61,13 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(cn);
         } catch (SchedulerException se) {
             se.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -60,9 +75,23 @@ public class AlertRabbit {
      * Действие, выполняемое JobDetail.
      */
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println("Rabbit runs here ...");
+        }
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connect");
+            String tableName = "rabbit";
+            String sql = String.format("INSERT INTO %s(created_date) VALUES(?);",
+                    tableName
+            );
+            try (PreparedStatement s = cn.prepareStatement(sql)) {
+                s.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                s.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -70,13 +99,32 @@ public class AlertRabbit {
      * Метод выполняет чтение файла настроек и отдаёт их наружу
      * @return Объект Properties представляющий настройки для SimpleScheduleBuilder.
      * @throws IOException если происходит ошибка при чтении файла настроек.
+     * @throws ClassNotFoundException
+     * @throws SQLException
      */
-    private static Properties loadProp() throws IOException{
+    private static Properties loadProp() throws IOException, ClassNotFoundException, SQLException {
         Properties tmp = new Properties();
         try (InputStream fileInputStream = new FileInputStream("./src/main/resources/rabbit.properties")) {
             tmp.load(fileInputStream);
         }
 
         return tmp;
+    }
+
+    /**
+     *
+     * @param prop
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    private static Connection dbConnection(Properties prop) throws ClassNotFoundException, SQLException {
+        Class.forName(prop.getProperty("jdbc.driver"));
+
+        return DriverManager.getConnection(
+                prop.getProperty("jdbc.url"),
+                prop.getProperty("jdbc.username"),
+                prop.getProperty("jdbc.password")
+        );
     }
 }
