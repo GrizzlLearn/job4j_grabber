bafbf1c48219194eed5ffccfa2017c7ab2c6ec59
GrizzlLearn <dylebedev.learning@gmail.com>
2023-09-29 21:05:31 +0300

    WIP: 2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,13 +5,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
+    private final DateTimeParser dateTimeParser;
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
@@ -19,7 +24,11 @@ public class HabrCareerParse {
 
     private static final String PAGE_NUMBER = String.format(PAGE_LINK + "%s", "?page=");
 
-    public static void main(String[] args) throws IOException {
+    HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    /*public static void main(String[] args) throws IOException {
         for (int i = 1; i <= 5; i++) {
             Connection connection = Jsoup.connect(String.format(PAGE_NUMBER + "%s", i));
             Document document = connection.get();
@@ -49,6 +58,36 @@ public class HabrCareerParse {
                 }
             });
         }
+    }*/
+
+
+    public static void main(String[] args) throws IOException {
+        HabrCareerParse hcp = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> result = new ArrayList<>();
+        List<String> linkResult = new ArrayList<>();
+        getVacancyLinks();
+
+        for (Post post : result) {
+            System.out.println(post.toString());
+        }
+    }
+
+    private static List<String> getVacancyLinks() {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            String sourceLink = String.format(PAGE_NUMBER + "%s", i);
+            Connection connection = Jsoup.connect(sourceLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                result.add(link);
+            });
+        }
+        return result;
     }
 
     private static String retrieveDescription(String link) throws IOException {
@@ -56,5 +95,30 @@ public class HabrCareerParse {
         Document document = connection.get();
         Elements body = document.select(".vacancy-description__text");
         return body.text();
+    }
+
+    private static List<Post> postBuilder() throws IOException {
+        List<Post> result = new ArrayList<>();
+        List<String> tmp = getVacancyLinks();
+        for (String link : tmp) {
+            Post post = new Post();
+            post.setLink(link);
+            post.setDescription(retrieveDescription(link));
+            result.add(post);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> result = new ArrayList<>();
+        try {
+            //result.add(postBuilder(link));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
