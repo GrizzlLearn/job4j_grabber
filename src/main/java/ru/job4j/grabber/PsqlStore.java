71ccc887ce99a633e9195201c8bb89e7e186849d
GrizzlLearn <dylebedev.learning@gmail.com>
2023-10-01 17:39:51 +0300

    WIP: 5. PsqlStore [#285209] fix add private method to create post object

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -29,7 +29,9 @@ public class PsqlStore implements Store {
 
     /**
      * Метод принимает объект Post и добавляет его в БД
+     *
      * @param post объект Post
+     * @see Post
      */
     @Override
     public void save(Post post) {
@@ -51,22 +53,19 @@ public class PsqlStore implements Store {
     }
 
     /**
-     * Метод возвращает список всех объектов Post из ЬД
+     * Метод возвращает список всех объектов Post из БД
+     *
      * @return список объектов Post
+     * @see List
+     * @see Post
      */
     @Override
     public List<Post> getAll() {
         List<Post> result = new ArrayList<>();
         String sql = String.format("SELECT * FROM %s", TABLE_NAME);
         try (PreparedStatement ps = cnn.prepareStatement(sql); ResultSet resultSet = ps.executeQuery()) {
-            while (resultSet.next()) {
-                Post post = new Post();
-                post.setId(resultSet.getInt(1));
-                post.setTitle(resultSet.getString(2));
-                post.setDescription(resultSet.getString(3));
-                post.setLink(resultSet.getString(4));
-                post.setCreated(resultSet.getTimestamp(5).toLocalDateTime().withNano(0));
-                result.add(post);
+            if (resultSet.isBeforeFirst()) {
+                result.addAll(createPostObjects(resultSet));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -76,9 +75,36 @@ public class PsqlStore implements Store {
     }
 
     /**
+     * Метод принимает объект ResultSet и из его данных создаёт объект Post и добавляет его список.
+     *
+     * @param resultSet объект ResultSet
+     * @return список объектов Post
+     * @throws SQLException если тип данных в ResultSet не может быть преобразован в запрашиваемый
+     * @see ResultSet
+     * @see List
+     * @see Post
+     */
+    private List<Post> createPostObjects(ResultSet resultSet) throws SQLException {
+        List<Post> result = new ArrayList<>();
+        while (resultSet.next()) {
+            Post tmp = new Post();
+            tmp.setId(resultSet.getInt(1));
+            tmp.setTitle(resultSet.getString(2));
+            tmp.setDescription(resultSet.getString(3));
+            tmp.setLink(resultSet.getString(4));
+            tmp.setCreated(resultSet.getTimestamp(5).toLocalDateTime().withNano(0));
+            result.add(tmp);
+        }
+
+        return result;
+    }
+
+    /**
      * Метод принимает id объект и производит его поиск в БД
+     *
      * @param id Объекта Post
      * @return найденный объект Post или null, если объект не найден
+     * @see Post
      */
     @Override
     public Post findById(int id) {
@@ -88,13 +114,8 @@ public class PsqlStore implements Store {
         try (PreparedStatement ps = cnn.prepareStatement(sql); ) {
             ps.setInt(1, id);
             try (ResultSet resultSet = ps.executeQuery()) {
-                while (resultSet.next()) {
-                    result = new Post();
-                    result.setId(resultSet.getInt(1));
-                    result.setTitle(resultSet.getString(2));
-                    result.setDescription(resultSet.getString(3));
-                    result.setLink(resultSet.getString(4));
-                    result.setCreated(resultSet.getTimestamp(5).toLocalDateTime().withNano(0));
+                if (resultSet.isBeforeFirst()) {
+                    result = createPostObjects(resultSet).get(0);
                 }
             }
         } catch (SQLException e) {
